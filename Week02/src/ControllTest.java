19352cffa7be132d9077b66e1e7ba189855d7cdf
DS <DS@DESKTOP-J1HK6TQ>
2023-09-11 20:08:24 +0900

    test

---
+++ Week02/src/ControllTest.java
@@ -0,0 +1,21 @@
+interface Controllable  {
+	void play();
+	void stop();
+}
+
+public class ControllTest {
+	public static void main(String arg[]) {
+		Controllable c = new Controllable() {
+			public void play() {
+				System.out.println("PLAY");
+			}
+
+			public void stop() {
+				System.out.println("STOP");
+			}
+			
+		};
+		c.play();
+		c.stop();
+	}
+}
